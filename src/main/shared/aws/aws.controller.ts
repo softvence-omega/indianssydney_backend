ab263342dbe8adbf9f551c23cb1c32b535a5aa73
Joy43 <ssjoy43@gmail.com>
2025-09-23 10:38:21 +0600

    aws error fixed

--- src/main/shared/aws/aws.controller.ts
+++ src/main/shared/aws/aws.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Query, BadRequestException } from '@nestjs/common';
 import { awsService } from './aws.service';
 
 @Controller('s3')
@@ -6,8 +6,12 @@ export class awsController {
   constructor(private readonly s3Service: awsService) {}
 
   @Get('aws-upload-url')
-  async getUploadURL() {
-    const { uploadURL, key } = await this.s3Service.generateUploadURL();
+  async getUploadURL(@Query('fileType') fileType: string) {
+    if (!fileType) {
+      throw new BadRequestException('fileType query param is required');
+    }
+
+    const { uploadURL, key } = await this.s3Service.generateUploadURL(fileType);
     return { uploadURL, key };
   }
 }
